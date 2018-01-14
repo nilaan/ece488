38f3b71143f30e1f4f8c1c1287bcedfa3886b5f4
nilaan <g.nilaan@gmail.com>
2018-01-14 12:05:24 -0500

    update input function

--- code/lecture_1_12.m
+++ code/lecture_1_12.m
@@ -11,13 +11,13 @@ close all;
 %%
 syms th thd thdd T t
 
-tspan = [0 1];
+tspan = [0 10];
 
 x = [th; thd];
 u = T;
 
-x0 = [0; 0];
-xbar = [1; 0];
+x0 = [0.1; 0];
+xbar = [0; 0];
 ubar = [0];
 
 n = length(x); % states
@@ -27,10 +27,9 @@ m = length(u); % inputs
 f_eq = thdd+thd+sin(th) == T;
 f(1) = thd;
 f(2) = solve(f_eq, thdd);
+u_t = @(t) (heaviside(t)-heaviside(t-0.1));
 
-u_t = (heaviside(t)-heaviside(t-0.1));
-
-[t1, x1] = ode45(@(t1, x1) nonLinearFunc(f, x, x1, u, u_t, t1), tspan, x0);
+[t1, x1] = ode45(@(t1, x1) nonLinearFunc(f, x, x1, u, u_t(t1), t1), tspan, x0);
 figure, plot(t1, x1(:, 1));
 
 %Linearlize
@@ -40,8 +39,7 @@ B = subs(jacobian(f, u), u, ubar);
 A_t = double(A);
 B_t = double(B);
 
-
-[t2, x2] = ode45(@(t, x2)A_t*x2+B_t*u_t, tspan, x0);
+[t2, x2] = ode45(@(t2, x2)A_t*x2+B_t*u_t(t2), tspan, x0);
 figure, plot(t2, x2(:, 1));
 
 
